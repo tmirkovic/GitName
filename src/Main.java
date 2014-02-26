f7157e4828a5636117b7bff2e36498b81008be78
ndgreen <github@nathangreen.com>
2014-02-26 15:06:19 -0500

    reset Users

--- src/Main.java
+++ src/Main.java
@@ -6,26 +6,16 @@ public class Main {
 private ArrayList<Student> students= new ArrayList<Student>();
 private ArrayList<Team> teams= new ArrayList<Team>();
 	public Main() {
-		//ADD YOURSELF HERE with your github email and username
+		//ADD YOURSELF HERE with your Name, github email, and github username
 		Student nathan = new Student("Nathan Green", "github@nathangreen.com", "ndgreen");
 		nathan.canNotMeet(Day.SATURDAY);//Choose only the days you cannot meet
 		nathan.canNotMeet(Day.SUNDAY);
 		nathan.preferTimeToMeet(Time.DAY); // Choose Day/Night/Either
 		addStudent(nathan);
 		
-		//Add yourself below here
+		//Add yourself below here, Do not delete other Users
 		
-		Student tina = new Student("Septina Dian Larasati", "septina.larasati@gmail.com", "slarasati");
-		tina.canNotMeet(Day.SATURDAY);//Choose only the days you cannot meet
-		tina.canNotMeet(Day.SUNDAY);
-		tina.preferTimeToMeet(Time.DAY); // Choose Day/Night/Either
-		addStudent(tina);
-		
-		Student tim = new Student("Tim Green", "github@nathangreen.com", "ndgreen");
-		tim.canNotMeet(Day.THURSDAY);//Choose only the days you cannot meet
-		tim.canNotMeet(Day.FRIDAY);
-		tim.preferTimeToMeet(Time.EITHER); // Choose Day/Night/Either
-		addStudent(tim);
+	
 		
 		
 		
